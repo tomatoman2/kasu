8511cc9fabbd6abe4bfe3d86189f7a92238340af
tomatoman2 <kasuga01m@gmail.com>
2020-05-03 01:33:50 +0900

    Add:お気に入り機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
-  has_many :messages
-  has_many :comments
+  has_many :messages, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_messages, through: :likes, source: :message
+
+  #ユーザーが投稿に対して既にいいねしているか
+  def already_liked?(message)
+    likes.exists?(message_id: message.id)
+  end
   validates :name, presence: true, uniqueness: true
   mount_uploader :avatar, AvatarUploader
 end
